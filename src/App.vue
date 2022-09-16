a3b820c31fbd18ab1c26b221b51b6f41f11827e0
Austin Brain <cybrainfoxy@gmail.com>
2022-09-16 01:46:48 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import '@/assets/css/fontawesome.min.css'
+import '@/assets/css/bootstrap.min.css'
+import '@/assets/css/style.css'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="avatar">
+        <img src="@/assets/img/avatar.png" />
+      </div>
+      <div class="nickname">
+        <h1>Ninja <i class=""></i></h1>
+      </div>
+    </div>
+    <div class="row button-containers">
+      <a
+        href="http://youtube.com/ninja?sub_confirmation=1"
+        class="btn bg-white buttons"
+        ><span class="fab fa-youtube"></span> <span>Ninja Gaming</span></a
+      >
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+.avatar {
+  width: 150px;
+  height: 150px;
+  border-radius: 150px;
+  margin: 20px auto;
+}
+.avatar img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+.nickname {
+  color: #fff;
+}
+.buttons {
+  width: 100%;
+  color: #000;
+}
+</style>
